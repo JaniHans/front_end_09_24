25bebee398af0482f56ab22218db19fdc639874c
JaniHans <laurihanstin@gmail.com>
2024-10-09 11:30:40 +0200

    test

---
+++ kalkulaator/src/App.js
@@ -0,0 +1,42 @@
+import { useRef, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [vastus, muudaVastus] = useState(0);
+  const input1Ref = useRef();
+  const input2Ref = useRef();
+  
+  function korruta() {
+    muudaVastus(input1Ref.current.value * input2Ref.current.value);
+  }
+
+  function lahuta() {
+    muudaVastus(input1Ref.current.value - input2Ref.current.value);
+  }
+
+  function jaga() {
+    muudaVastus(input1Ref.current.value / input2Ref.current.value);
+  }
+  
+  function liida() {
+    muudaVastus(parseFloat(input1Ref.current.value) + parseFloat(input2Ref.current.value));
+  }
+  
+  
+  return (
+
+    <div className="App">
+      <input ref={input1Ref} type="text" />
+      <input ref={input2Ref} type="text" />
+      <br/>
+      {vastus}  <br/>
+      <button onClick={korruta}>KORRUTA</button>
+      <button onClick={lahuta}>KORRUTA</button>
+      <button onClick={jaga}>JAGA</button>
+      <button onClick={liida}>LIIDA</button>
+      
+    </div>
+  );
+}
+
+export default App;
