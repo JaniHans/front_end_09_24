99f067c4063baa0261429b1b2e2a0b9d42ea2a3b
JaniHans <laurihanstin@gmail.com>
2024-10-09 16:55:26 +0200

    test

--- osa2/src/pages/SõneUurimine.js
+++ osa2/src/pages/SõneUurimine.js
@@ -1,21 +1,30 @@
 import { useState, useRef } from "react" 
 function SõneUurimine() {
     const [sonum, muudaNimi] = useState("")
-    const nimi = useRef()
+    const nimiRef = useRef()
 
     function lisaNimi() {
-        const nimiValue = nimi.current.value;
+        if (nimiRef.current.value === "") {
+            muudaNimi("Pead sisestama nime!");
+            return;
+        }
 
-        muudaNimi(nimiValue);
+        if (nimiRef.current.value[3] !== "o") {
+            muudaNimi("Neljas täht peab olema 'o");
+            return;
+        }
+
+        muudaNimi("Nimi lisatud");
     }
-
   return (
     <div>
         <div className="container1">
         <label >Sisesta nimi</label><br/>
-        <input ref={nimi} type="text" /> <br/>
-        <button className={nimi.current?.value?.[3] === "o" ? {nimi} : "Nimi ei ole korrektne"} onClick={lisaNimi}>Muuda nimi</button><br/>
+        <input ref={nimiRef} type="text" /> <br/>
+        <button onClick={lisaNimi}>Lisa nimi</button><br/><br/>
         <div>{sonum}</div>
+        <div>{}</div>
+        
         </div>
     </div>
 
